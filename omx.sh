9f93b3cc4d60b853f6be99ff176ecd88db9537f0
j-mir-prazak <j.mir.prazak@gmail.com>
2018-09-06 00:15:48 +0200

    omx.sh lists through /media/*/*/rpi_player; if dir exists, sets up the variable for videolocations and is flashdrive player. xset dpms on again

--- omx.sh
+++ omx.sh
@@ -4,15 +4,20 @@ cd "$CURDIR"
 # set here the path to the directory containing your videos
 VideoLocation="./assets"
 
-if [ -d /media/pi/*/rpi_player ]
-then
-  VideoLocation="/media/pi/"*"/rpi_player/assets"
-  echo $VideoLocation
-fi
+
+for i in /media/*/*/rpi_player
+do
+  if [ -d $i ]
+  then
+    VideoLocation=/media/*/*/rpi_player/assets
+  fi
+done
+echo $VideoLocation
+
 # you can probably leave this alone
 Process="omxplayer"
 # our loop
-xset dpms force off
+#xset dpms force off
 while true; do
         if ps ax | grep -v grep | grep $Process > /dev/null
         then
@@ -21,11 +26,11 @@ while true; do
 else
         for entry in $VideoLocation/*
         do
-		    xset dpms force off
+		    #xset dpms force off
                 # -r for stretched over the entire location
 		    echo "$entry"
         omxplayer -b -o both "$entry" > /dev/null
-		    xset dpms force off
+		    #xset dpms force off
         done
 fi
 done
