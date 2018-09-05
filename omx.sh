5a8203b2d8e12b7de416b0a5556fcf08b0a1bb9c
j-mir-prazak <j.mir.prazak@gmail.com>
2018-09-06 00:02:58 +0200

    quick

--- omx.sh
+++ omx.sh
@@ -6,13 +6,13 @@ VideoLocation="./assets"
 
 if [ -d /media/pi/*/rpi_player ]
 then
-  VideoLocation=/media/pi/*/rpi_player/assets
+  VideoLocation="/media/pi/"*"/rpi_player/assets"
   echo $VideoLocation
 fi
 # you can probably leave this alone
 Process="omxplayer"
 # our loop
-#xset dpms force off
+xset dpms force off
 while true; do
         if ps ax | grep -v grep | grep $Process > /dev/null
         then
@@ -21,11 +21,11 @@ while true; do
 else
         for entry in $VideoLocation/*
         do
-		    #xset dpms force off
+		    xset dpms force off
                 # -r for stretched over the entire location
 		    echo "$entry"
         omxplayer -b -o both "$entry" > /dev/null
-		    #xset dpms force off
+		    xset dpms force off
         done
 fi
 done
